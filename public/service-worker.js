cb2fd5d555e353d9871ff080c517ba797563de2c
Светлана <sorlana250980@google.com>
2026-06-14 18:54:16 +0300

    fix: bump SW cache version to v7 to load updated app.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@
  * - Offline-fallback страница
  */
 
-const CACHE_NAME = 'traking-v6';
+const CACHE_NAME = 'traking-v7';
 const OFFLINE_URL = '/offline.html';
 
 // Ресурсы для предварительного кэширования
