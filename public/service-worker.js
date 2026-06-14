02cd5544bc2b83bbd412f6ffc0ab819e33e548bc
Светлана <sorlana250980@google.com>
2026-06-14 21:58:14 +0300

    fix: even larger fonts on mobile

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@
  * - Offline-fallback страница
  */
 
-const CACHE_NAME = 'traking-v12';
+const CACHE_NAME = 'traking-v13';
 const OFFLINE_URL = '/offline.html';
 
 // Ресурсы для предварительного кэширования
