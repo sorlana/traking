00e5ca51fd1a286a67eaeeb00849129a2d78fbfd
Светлана <sorlana250980@google.com>
2026-06-15 13:57:14 +0300

    fix: gray buttons directly in template classes instead of CSS hack

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@
  * - Offline-fallback страница
  */
 
-const CACHE_NAME = 'traking-v55';
+const CACHE_NAME = 'traking-v56';
 const OFFLINE_URL = '/offline.html';
 
 // Ресурсы для предварительного кэширования
