a362b8ef0a5f089bae62b6e3215920638635e00d
Светлана <sorlana250980@google.com>
2026-07-16 17:53:14 +0300

    fix: учтена высота нижней навпанели для чата на мобильном

--- views/tasks/show.php
+++ views/tasks/show.php
@@ -311,7 +311,7 @@ $isClosed = ($task['status_code'] === 'closed');
     }
     @media (max-width: 1023px) {
         body { height: 100vh; overflow: hidden; }
-        main { overflow: hidden; height: calc(100vh - 2.5rem); padding-bottom: 0 !important; padding-left: 0 !important; padding-right: 0 !important; display: flex !important; flex-direction: column !important; }
+        main { overflow: hidden; height: calc(100vh - 2.5rem - 3.5rem); padding-bottom: 0 !important; padding-left: 0 !important; padding-right: 0 !important; display: flex !important; flex-direction: column !important; }
     }
 </style>
 
